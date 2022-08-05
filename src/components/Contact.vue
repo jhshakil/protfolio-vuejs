4b44ab8c68864127ef7bb3a18c9b2769c8159d46
Shakil <shakil11271@gmail.com>
2022-08-05 10:01:07 +0600

    complete contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,22 +2,22 @@
     <div class="m-16 bg-gray-300 rounded-lg">
         <h2 class="text-4xl font-bold text-center pt-8">Contact Me</h2>
         <div class="grid grid-cols-2 gap-8 items-center">
-            <form class="p-8" @submit.prevent="sending()">
+            <form ref="form" class="p-8" @submit.prevent="sending()">
                 <div class="my-4">
                     <label>Name</label>
-                    <input type="text" v-model="user.name" 
+                    <input type="text" v-model="user.name" name='user_name' 
                     class="border outline-none rounded border-black p-2 w-full" 
                     placeholder="Enter Your Name" required>
                 </div>
                 <div class="my-4">
                     <label>Email</label>
-                    <input type="email" v-model="user.email" 
+                    <input type="email" v-model="user.email" name='user_email' 
                     class="border outline-none rounded border-black p-2 w-full"
                     placeholder="Enter Your Email" required>
                 </div>
                 <div class="my-4">
                     <label>Message</label>
-                    <input type="textarea" v-model="user.message" 
+                    <input type="textarea" v-model="user.message" name='message' 
                     class="border outline-none rounded border-black p-2 w-full" 
                     placeholder="Enter Your Message" required>
                 </div>
@@ -36,6 +36,8 @@
 </template>
 
 <script>
+import emailjs from '@emailjs/browser';
+
 export default {
     name: 'contactMe',
     data: function(){
@@ -49,7 +51,16 @@ export default {
     },
     methods: {
         sending: function( ) {
-            console.log(this.user)
+            console.log(this.$refs.form)
+            emailjs.sendForm('service_a0bsj82', 'template_k26yajd', this.$refs.form, 'OCqvS9D0ZS5iL2fkz')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+            this.user.name = '';
+            this.user.email = '';
+            this.user.message = '';
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
         }
     }
 }
--- src/components/MyProject.vue
+++ src/components/MyProject.vue
@@ -7,7 +7,9 @@
                 <h3 class="text-2xl font-bold text-center my-4">Manufacture Website</h3>
                 <p class="text-lg">This is a manufacturer website. It is a MERN Stack project. This website is controlled by Admin. Admin can add products and manage orders. Here client site code and server site code</p>
                 <div class="flex justify-evenly items-center my-4">
-                    <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">Live site</div>
+                    <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">
+                        <a target="_blank" href="https://shakils-hardware.web.app/">Live Site</a>
+                    </div>
                     <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">Details</div>
                 </div>
             </div>
@@ -18,7 +20,9 @@
                 <h3 class="text-2xl font-bold text-center my-4">Warehouse Management Website</h3>
                 <p class="text-lg">This is a Warehouse Management Website. It is a MERN Stack project. Add product, Add Quantity and Delete Product are the main feature in this website. Here client site code and server site code</p>
                 <div class="flex justify-evenly items-center my-4">
-                    <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">Live site</div>
+                    <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">
+                        <a target="_blank" href="https://car-dealer-af8e9.web.app/">Live Site</a>
+                    </div>
                     <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">Details</div>
                 </div>
             </div>
@@ -29,7 +33,9 @@
                 <h3 class="text-2xl font-bold text-center my-4">Randomly Collect Product</h3>
                 <p class="text-lg">This is a Javascript project. This project people can add some product and choose randomly one product by clicking a button. Here code</p>
                 <div class="flex justify-evenly items-center my-4">
-                    <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">Live site</div>
+                    <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">
+                        <a target="_blank" href="https://meek-belekoy-cc7525.netlify.app/">Live Site</a>
+                    </div>
                     <div class="bg-gray-300 text-black p-2 rounded-md w-1/4 text-xl text-center">Details</div>
                 </div>
             </div>
