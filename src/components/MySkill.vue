a663c70b04734b2fb2b4cc7f7c3a3c4c9324ea41
Shakil <shakil11271@gmail.com>
2022-11-14 23:25:29 +0600

    add skill

--- src/components/MySkill.vue
+++ src/components/MySkill.vue
@@ -1,48 +1,94 @@
 <template>
-    <div class="my-12 lg:my-16 mx-2 lg:mx-16">
-        <h2 class="text-2xl lg:text-4xl font-bold text-center">My Skill</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
-            <div class="bg-gray-200 shadow-lg rounded-lg p-4 lg:p-8">
-                <h3 class="text-xl lg:text-2xl font-bold text-center">Frontend</h3>
-                <div class="grid grid-cols-4 lg:grid-cols-3 gap-4 my-4 lg:my-8">
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Javascript</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">React</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Redux</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Vue</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Bootstrap</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Tailwind</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">HTML5</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">CSS3</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">DaisyUI</div>
-                </div>
-            </div>
-            <div class="bg-gray-200 shadow-lg rounded-lg p-4 lg:p-8">
-                <h3 class="text-xl lg:text-2xl font-bold text-center">Backend</h3>
-                <div class="grid grid-cols-4 lg:grid-cols-3 gap-4 my-4 lg:my-8">
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Node</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Express</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">MongoDB</div>
-                </div>
-            </div>
-            <div class="bg-gray-200 shadow-lg rounded-lg p-4 lg:p-8">
-                <h3 class="text-xl lg:text-2xl font-bold text-center">Tools</h3>
-                <div class="grid grid-cols-4 lg:grid-cols-3 gap-4 my-4 lg:my-8">
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Firebase</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Figma</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Github</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Heroku</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">React-Query</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">React-Form</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Netlify</div>
-                <div class="font-bold rounded-md text-sm lg:text-lg text-center">Photopea</div>
-                </div>
-            </div>
+  <div class="my-12 lg:my-16 mx-2 lg:mx-16">
+    <h2 class="text-2xl lg:text-4xl font-bold text-center">My Skill</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
+      <div class="bg-gray-200 shadow-lg rounded-lg p-4 lg:p-8">
+        <h3 class="text-xl lg:text-2xl font-bold text-center">Frontend</h3>
+        <div class="grid grid-cols-4 lg:grid-cols-3 gap-4 my-4 lg:my-8">
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Javascript
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            React
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Redux
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Vue
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Nuxt
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Bootstrap
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Tailwind
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            HTML5
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            CSS3
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            DaisyUI
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Vuetify
+          </div>
         </div>
+      </div>
+      <div class="bg-gray-200 shadow-lg rounded-lg p-4 lg:p-8">
+        <h3 class="text-xl lg:text-2xl font-bold text-center">Backend</h3>
+        <div class="grid grid-cols-4 lg:grid-cols-3 gap-4 my-4 lg:my-8">
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Node
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Express
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            MongoDB
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-200 shadow-lg rounded-lg p-4 lg:p-8">
+        <h3 class="text-xl lg:text-2xl font-bold text-center">Tools</h3>
+        <div class="grid grid-cols-4 lg:grid-cols-3 gap-4 my-4 lg:my-8">
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Firebase
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Figma
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Github
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Heroku
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            React-Query
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            React-Form
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Netlify
+          </div>
+          <div class="font-bold rounded-md text-sm lg:text-lg text-center">
+            Photopea
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'MySkill'
-}
+  name: "MySkill",
+};
 </script>
